d47f4aa37224e1ec67bf923262e4ece98357f7a1
wpzero <wangping@galiumsof.com>
2015-04-17 15:24:22 +0800

    修改page no不是integer的情况

--- lib/wfl_paginate/paginate.rb
+++ lib/wfl_paginate/paginate.rb
@@ -7,9 +7,9 @@ module WflPaginate
 		included do
 			scope :paginate, ->(page_infos){
 				if page_infos[:page_no]
-					per_page = page_infos[:per_page]
+					per_page = page_infos[:per_page].to_i
 					per_page ||= WflPaginate.config.per_page
-					offset((page_infos[:page_no]-1)*per_page).limit(per_page)
+					offset((page_infos[:page_no].to_i-1)*per_page).limit(per_page)
 				else
 					scoped
 				end
